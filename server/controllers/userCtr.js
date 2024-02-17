f400ba40bd52af0ada947efcb96af4f295bb103d
GharbiHaithem <gharbi.haythem1988@gmail.com>
2024-02-18 00:31:34 +0100

    add bcryptjs2

--- server/controllers/userCtr.js
+++ server/controllers/userCtr.js
@@ -1,6 +1,6 @@
 const User = require('../models/user')
 const {generateToken}  = require('../config/generateToken')
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs')
 const userCtrl = {
     saveOrLoginUser: async (req, res) => {
         const data = {
