3fe161fc882e9fac4c877812b388b90d568aa51e
GharbiHaithem <gharbi.haythem1988@gmail.com>
2024-02-18 02:05:45 +0100

    update22

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const recetteRoute = require('./routers/recette.route')
 
 const app = express()
 app.use(cors({
-    origin:"https://note-three-kappa.vercel.app",
+    origin:"http://localhost:5173",
    
     credentials:true
 }))
