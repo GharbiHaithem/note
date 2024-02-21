d67f29116660cca4b495cba83994b31626e0190f
GharbiHaithem <gharbi.haythem1988@gmail.com>
2024-02-21 03:16:18 +0100

    007

--- server/middlware/upload.image.js
+++ server/middlware/upload.image.js
@@ -3,14 +3,15 @@ const multer =  require('multer')
 const path = require('path')
 const fs = require('fs')
 const multerStorage = multer.diskStorage({
-    destination:function(req,file,cb){
-cb(null,path.join(__dirname,'../public/images'))
+    destination: function (req, file, cb) {
+      // Vous n'avez pas besoin de définir un dossier local, car les fichiers iront directement à Cloudinary
+      cb(null, '');
     },
-    filename:function(req,file,cb){
-const suffixUnique = Date.now() + "-" + Math.round(Math.random())* 1e9;
-cb(null,file.fieldname+ "-" + suffixUnique + ".jpg") 
-    }
-})
+    filename: function (req, file, cb) {
+      const suffixUnique = Date.now() + '-' + Math.round(Math.random()) * 1e9;
+      cb(null, file.fieldname + '-' + suffixUnique + '.jpg');
+    },
+  });
 const multerFilter=(req,file,cb)=>{
 if(file.mimetype.startsWith('image')){
     cb(null,true)
